c1360e1edd3dd01625139f998a1db92957c11aae
Maria Paula Duque <92931247+Mariaduque22@users.noreply.github.com>
2022-04-25 21:29:17 -0500

    parte 4.1

--- src/main/java/src/zooAnimales/Reptil.java
+++ src/main/java/src/zooAnimales/Reptil.java
@@ -5,21 +5,68 @@ import gestion.Zona;
 
 public class Reptil extends Animal {
 	private static ArrayList <Reptil> listado=new ArrayList<Reptil>();
-	public int iguanas;
-	public int serpientes;
+	public static int iguanas;
+	public static int serpientes;
 	private String colorEscamas;
 	private int largoCola;
 	
 	public Reptil() {
 	}
 
-	public Reptil(int totalAnimales, String nombre, int edad, String habitat, String genero, Zona zona, Reptil listado,
-			int iguanas, int serpientes, String colorEscamas, int largoCola) {
-		super(totalAnimales, nombre, edad, habitat, genero, zona);
-		this.listado = listado;
-		this.iguanas = iguanas;
-		this.serpientes = serpientes;
+
+	public Reptil(String nombre, int edad, String habitat, String genero, String colorEscamas,
+			int largoCola) {
 		this.colorEscamas = colorEscamas;
+		this.largoCola = largoCola;
+	}
+
+
+	public static int cantidadReptiles() {
+		return Reptil.listado.size();
+	}
+	
+	@Override 
+	public String movimiento() {
+		return "reptar";
+	}
+	
+	public static Reptil crearSerpiente(String nombre, int edad, String genero) {
+		Reptil.serpientes +=1;
+		return new Reptil(nombre, edad, "jungla", genero, "blanco",1);
+	}
+	
+	public static Reptil crearIguana(String nombre, int edad, String genero) {
+		Reptil.iguanas +=1;
+		return new Reptil(nombre,edad,"humedal",genero,"verde",3);
+	}
+
+
+	public static ArrayList<Reptil> getListado() {
+		return listado;
+	}
+
+
+	public static void setListado(ArrayList<Reptil> listado) {
+		Reptil.listado = listado;
+	}
+
+
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+
+
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas = colorEscamas;
+	}
+
+
+	public int getLargoCola() {
+		return largoCola;
+	}
+
+
+	public void setLargoCola(int largoCola) {
 		this.largoCola = largoCola;
 	}
 
