0f520a07b1ddb9ad5b1bb007a2929802964057d4
Maria Paula Duque <92931247+Mariaduque22@users.noreply.github.com>
2022-04-25 20:08:07 -0500

    parte1.1

--- src/main/java/src/gestion/Zoologico.java
+++ src/main/java/src/gestion/Zoologico.java
@@ -1,5 +1,24 @@
 package gestion;
 
 public class Zoologico {
-
+	private String nombre;
+	private String ubicacion;
+	private Zona zonas;
+	
+	public int cantidadTotalAnimales() {		/* no se que va*/
+		return 0;
+	}
+	public void agregarZonas() { 				/* no se que va*/
+	}    
+	
+	public Zoologico() {
+	}
+	
+	public Zoologico(String nombre, String ubicacion, Zona zonas) {
+		super();
+		this.nombre = nombre;
+		this.ubicacion = ubicacion;
+		this.zonas = zonas;
+	}
+		
 }
